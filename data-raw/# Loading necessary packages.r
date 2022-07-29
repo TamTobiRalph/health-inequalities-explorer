30bf101893e7022fdb713a6d6e8e29c012387486
TamTobiRalph <ralphtamtobi@hotmail.com>
2022-07-29 12:45:01 +0100

    Cleaned Up Code

---
+++ data-raw/# Loading necessary packages.r
@@ -0,0 +1,214 @@
+# Loading necessary packages
+library(tidyverse)
+library(httr)
+library(readxl)
+library(corrr)
+library(janitor)
+library(dplyr)
+library(skimr)
+
+#load url links
+april_data <- "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2022/05/Daily-Discharge-SitRep-Monthly-Data-Web-File-April2022-2.xlsx"
+may_data <- "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2022/06/Daily-Discharge-SitRep-Monthly-Data-Web-File-May2022.xlsx"
+
+# Load April raw data
+ GET(
+      april_data,
+    write_disk(april <- tempfile(fileext = ".xlsx"))
+  )
+
+
+april_raw <-
+  read_excel(
+    april,
+    sheet = "Table 2",
+    skip = 59
+  )
+
+# Select and rename relevant columns
+april_selected <-
+  april_raw|>
+  select(Region, `Org Code`, `Org Name`,
+          "2022-4-1"= "...4", "2022-4-2"= "...8","2022-4-3"= "...12",
+                      "2022-4-4"= "...16", "2022-4-5"= "...20","2022-4-6"= "...24","2022-4-7"= "...28",
+                      "2022-4-8"= "...32", "2022-4-9"= "...36","2022-4-10"= "...40","2022-4-11"= "...44",                
+                      "2022-4-12"= "...48", "2022-4-13"= "...52","2022-4-14"= "...56","2022-4-15"= "...60",
+                      "2022-4-16"= "...64", "2022-4-17"= "...68","2022-4-18"= "...72","2022-4-19"= "...76",
+                      "2022-4-20"= "...80", "2022-4-21"= "...84","2022-4-22"= "...88","2022-4-23"= "...92",                
+                      "2022-4-24"= "...96", "2022-4-25"= "...100","2022-4-26"= "...104","2022-4-27"= "...108",
+                      "2022-4-28"= "...112","2022-4-29"= "...116","2022-4-30"= "...120"
+        )
+
+
+#Pivot longer all the date column, get the mean and sum for each trust
+april_pivot <- april_selected |> 
+            pivot_longer(cols = !c(Region, `Org Code`, `Org Name`)) |>
+            group_by(`Org Code`) |>
+            mutate(april_mean = mean(value)) |>
+            mutate(april_sum = sum(value)) |>
+            distinct(Region, `Org Code`, `Org Name`, april_mean, april_sum) |>
+            ungroup()
+
+# Load May raw data
+
+ GET(
+      may_data,
+    write_disk(may <- tempfile(fileext = ".xlsx"))
+  )
+
+
+may_raw <-
+  read_excel(
+    may,
+    sheet = "Table 2",
+    skip = 58
+  )
+
+# Drop columns ( Selecting Number of patients who no longer meet the criteria to reside)
+may_selected <-
+  may_raw|>
+  select(Region, `Org Code`, `Org Name`,
+          "2022-5-1"= "...4", "2022-5-2"= "...8","2022-5-3"= "...12",
+                      "2022-5-4"= "...16", "2022-5-5"= "...20","2022-5-6"= "...24","2022-5-7"= "...28",
+                      "2022-5-8"= "...32", "2022-5-9"= "...36","2022-5-10"= "...40","2022-5-11"= "...44",                
+                      "2022-5-12"= "...48", "2022-5-13"= "...52","2022-5-14"= "...56","2022-5-15"= "...60",
+                      "2022-5-16"= "...64", "2022-5-17"= "...68","2022-5-18"= "...72","2022-5-19"= "...76",
+                      "2022-5-20"= "...80", "2022-5-21"= "...84","2022-5-22"= "...88","2022-5-23"= "...92",                
+                      "2022-5-24"= "...96", "2022-5-25"= "...100","2022-5-26"= "...104","2022-5-27"= "...108",
+                      "2022-5-28"= "...112","2022-5-29"= "...116","2022-5-30"= "...120", "2022-5-31"= "...124"
+        )
+
+
+##Pivot longer all the date column, get the mean and sum for each trust
+may_pivot <- may_selected |> 
+            pivot_longer(cols = !c(Region, `Org Code`, `Org Name`)) |>
+            group_by(`Org Code`) |>
+            mutate(may_mean = mean(value)) |>
+            mutate(may_sum = sum(value)) |>
+            distinct(Region, `Org Code`, `Org Name`, may_mean, may_sum)|>
+            ungroup() 
+
+# Joining April and May discharge
+discharge <-
+  april_pivot |>
+  left_join(
+    may_pivot |> distinct(`Org Code`,may_mean, may_sum),
+    by = "Org Code"
+  )
+
+###A&E
+#Load A&E raw data
+
+april_ae <- "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2022/05/April-2022-AE-by-provider-Z1l86.xls"
+may_ae <- "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2022/06/May-2022-AE-by-provider-a5cdd.xls"
+june_ae <- "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2022/07/June-2022-AE-by-provider-4H6D4.xls"
+quater_ae <- "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2022/07/Quarter-1-2022-23-AE-by-provider-7W9zK.xls"
+
+GET(
+    april_ae,
+    write_disk(april_ae <- tempfile(fileext = ".xls"))
+  )
+
+raw_ae <-
+  read_excel(
+    april_ae,
+    sheet = "Provider Level Data",
+    skip = 15
+  )
+
+
+ae_sliced <-
+  raw_ae |>
+  slice(-(1:2))
+
+ae_select <- 
+    ae_sliced|>
+    select(`Org Code` = Code, `Total attendances`)
+
+
+GET(
+    may_ae,
+    write_disk(may_ae <- tempfile(fileext = ".xls"))
+  )
+
+
+may_ae <-
+  read_excel(
+    may_ae,
+    sheet = "Provider Level Data",
+    skip = 15
+  )
+
+may_ae_sliced <-
+  may_ae |>
+  slice(-(1:2))
+
+
+
+may_select <- 
+    may_ae_sliced|>
+    select(`Org Code` = Code, `May Total attendances` = `Total attendances` 
+    
+)
+
+GET(
+    june_ae,
+    write_disk(june_ae <- tempfile(fileext = ".xls"))
+  )
+
+june_ae <-
+  read_excel(
+    june_ae,
+    sheet = "Provider Level Data",
+    skip = 15
+  )
+
+june_ae_sliced <-
+  june_ae |>
+  slice(-(1:2))
+
+june_select <- 
+    june_ae_sliced|>
+    select(`Org Code` = Code, `June Total attendances`= `Total attendances` )
+
+GET(
+    quater_ae,
+    write_disk(quater_ae <- tempfile(fileext = ".xls"))
+  )
+
+quater_ae <-
+  read_excel(
+    quater_ae,
+    sheet = "Provider Level Data",
+    skip = 15
+  )
+
+quater_ae_sliced <-
+  quater_ae |>
+  slice(-(1:2))
+
+quater_select <- 
+    quater_ae_sliced|>
+    select(`Org Code` = Code, `Quater Total attendances`= `Total attendances` )
+
+
+discharge_ae <-
+  discharge |>
+  left_join(
+    ae_select |> distinct(`Org Code`, `Total attendances`),
+    by = "Org Code" ) |>
+    left_join(may_select |> distinct(`Org Code`, `May Total attendances`),
+    by = "Org Code" ) |>
+    left_join(june_select |> distinct(`Org Code`, `June Total attendances`),
+    by = "Org Code" ) |>
+    left_join(quater_select |> distinct(`Org Code`, `Quater Total attendances`),
+    by = "Org Code" )
+
+
+
+glimpse(discharge_ae)
+
+
+correlate(discharge_ae |>
+            select(where(is.double)))
+
--- data-raw/surge.r
+++ data-raw/surge.r
@@ -1,3 +1,9 @@
+### The lack of avalability of discharge data (which only has the month of April & May) makes it an uphill
+### task to create a model to predict surge durring the winter. The aim of this work below is to see other 
+### variables that are closesly correlated to discharge and have sufficient of historical data to create a model
+### to predict surge.
+
+
 # Loading necessary packages
 library(tidyverse)
 library(httr)
@@ -7,7 +13,7 @@ library(janitor)
 library(dplyr)
 library(skimr)
 
-#load url links
+#load url links for discharge data for April and May (June is not avalable at this time 29/7/2022)
 april_data <- "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2022/05/Daily-Discharge-SitRep-Monthly-Data-Web-File-April2022-2.xlsx"
 may_data <- "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2022/06/Daily-Discharge-SitRep-Monthly-Data-Web-File-May2022.xlsx"
 
@@ -17,7 +23,7 @@ may_data <- "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/20
     write_disk(april <- tempfile(fileext = ".xlsx"))
   )
 
-
+# Loading the data into our enviroment and selecting the neccessary rows
 april_raw <-
   read_excel(
     april,
@@ -46,21 +52,16 @@ april_pivot <- april_selected |>
             group_by(`Org Code`) |>
             mutate(april_mean = mean(value)) |>
             mutate(april_sum = sum(value)) |>
-            distinct(Region, `Org Code`, `Org Name`, april_mean, april_sum) 
-
-#Pivot longer all the date column, get the mean and sum for each trust
-april_mean <- april_pivot |>
-                  select(Region, `Org Code`, `Org Name`, april_mean)
-                  
+            distinct(Region, `Org Code`, `Org Name`, april_mean, april_sum) |>
+            ungroup()
 
 # Load May raw data
-
  GET(
       may_data,
     write_disk(may <- tempfile(fileext = ".xlsx"))
   )
 
-
+# Loading the data into our enviroment and selecting the neccessary rows
 may_raw <-
   read_excel(
     may,
@@ -83,44 +84,41 @@ may_selected <-
         )
 
 
-#
+##Pivot longer all the date column, get the mean and sum for each trust
 may_pivot <- may_selected |> 
             pivot_longer(cols = !c(Region, `Org Code`, `Org Name`)) |>
             group_by(`Org Code`) |>
             mutate(may_mean = mean(value)) |>
             mutate(may_sum = sum(value)) |>
-            distinct(Region, `Org Code`, `Org Name`, may_mean, may_sum) 
+            distinct(Region, `Org Code`, `Org Name`, may_mean, may_sum)|>
+            ungroup()
 
-may_mean <- may_pivot |>
-                  select(Region, `Org Code`, `Org Name`, may_mean)
-
-
-# April and May discharge
+# Joining April and May discharge
 discharge <-
   april_pivot |>
   left_join(
-    may_pivot |> distinct(may_mean, may_sum),
-    by = "Org Code"
-  )
-
-# April and May discharge that includes just mean
-discharge_mean <-
-  april_mean |>
-  left_join(
-    may_mean |> distinct(may_mean),
+    may_pivot |> distinct(`Org Code`, may_mean, may_sum),
     by = "Org Code"
   )
 
 ###Bed Occupancy
-#Load Bed occupancy url
+#Load Bed occupancy url for dfferent quaters
 bed_day <- "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2022/05/Beds-Open-Day-Only-Web_File-Q4-2021-22-Final-OIUJK.xlsx"
 bed_night <- "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2022/05/Beds-Open-Overnight-Web_File-Q4-2021-22-Final-OIUJK.xlsx"
- 
+bed_day_3 <- "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2022/05/Beds-Open-Day-Only-Web_File-Q3-2021-22-Final-QAZSD.xlsx"
+bed_night_3 <- "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2022/05/Beds-Open-Overnight-Web_File-Q3-2021-22-Final.xlsx"
+bed_day_2 <- "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2022/05/Beds-Open-Day-Only-Web_File-Q2-2021-22-Final.xlsx"
+bed_night_2 <- "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2022/05/Beds-Open-Overnight-Web_File-Q2-2021-22-Final.xlsx"
+bed_day_1 <- "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2022/05/Beds-Open-Day-Only-Web_File-Q1-2021-22-Final.xlsx"
+bed_night_1 <- "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2022/05/Beds-Open-Overnight-Web_File-Q1-2021-22-Final.xlsx"
+
+# Load bed day raw data for the 4th quater
  GET(
     bed_day,
     write_disk(bed_day <- tempfile(fileext = ".xlsx"))
   )
 
+# Loading the data into our enviroment and selecting the neccessary rows
 raw_day <-
   read_excel(
     bed_day,
@@ -133,7 +131,7 @@ beds_day_sliced <-
   raw_day |>
   slice(-(1:2))
 
-# Select cols
+# Select relevant columns
 beds_day_vars <-
   beds_day_sliced |>
   select(
@@ -142,11 +140,13 @@ beds_day_vars <-
     `general_acute_day_beds_occupied` = `General & Acute...13`
   )
 
+# Load bed night raw data for the 4th quater
  GET(
     bed_night,
     write_disk(bed_night <- tempfile(fileext = ".xlsx"))
   )
 
+# Loading the data into our enviroment and selecting the neccessary rows
 raw_night <-
   read_excel(
     bed_night,
@@ -168,13 +168,200 @@ beds_night_vars <-
     `general_acute_night_beds_occupied` = `General & Acute...13`
   )
 
-#Merging day and night bed together
+# Load bed day raw data for the 3rd quater
+ GET(
+    bed_day_3,
+    write_disk(bed_day_3 <- tempfile(fileext = ".xlsx"))
+  )
+
+# Loading the data into our enviroment and selecting the neccessary rows
+raw_day_3 <-
+  read_excel(
+    bed_day_3,
+    sheet = "NHS Trust by Sector",
+    skip = 14
+  )
+
+# remove first two entries (one is totals, other is blank)
+beds_day_sliced_3 <-
+  raw_day_3 |>
+  slice(-(1:2))
+
+# Select cols
+beds_day_vars_3 <-
+  beds_day_sliced_3 |>
+  select(
+    `Org Code`,
+    `3_total_beds_available_day`= `Total...6`,
+    `3_general_acute_day_beds_occupied` = `General & Acute...13`
+  )
+
+# Load bed night raw data for the 3rd quater
+GET(
+    bed_night_3,
+    write_disk(bed_night_3 <- tempfile(fileext = ".xlsx"))
+  )
+
+# Loading the data into our enviroment and selecting the neccessary rows
+raw_night_3 <-
+  read_excel(
+    bed_night_3,
+    sheet = "NHS Trust by Sector",
+    skip = 14
+  )
+
+# remove first two entries (one is totals, other is blank)
+beds_night_sliced_3 <-
+  raw_night_3 |>
+  slice(-(1:2))
+
+# Select cols
+beds_night_vars_3 <-
+  beds_night_sliced_3 |>
+  select(
+    `Org Code`,
+    `3_total_beds_available_night`= `Total...6`,
+    `3_general_acute_night_beds_occupied` = `General & Acute...13`
+  )
+  
+# Load bed day raw data for the 2nd quater
+GET(
+    bed_day_2,
+    write_disk(bed_day_2 <- tempfile(fileext = ".xlsx"))
+  )
+
+# Loading the data into our enviroment and selecting the neccessary rows
+raw_day_2 <-
+  read_excel(
+    bed_day_2,
+    sheet = "NHS Trust by Sector",
+    skip = 14
+  )
+
+# remove first two entries (one is totals, other is blank)
+beds_day_sliced_2 <-
+  raw_day_2 |>
+  slice(-(1:2))
+
+# Select cols
+beds_day_vars_2 <-
+  beds_day_sliced_2 |>
+  select(
+    `Org Code`,
+    `2_total_beds_available_day`= `Total...6`,
+    `2_general_acute_day_beds_occupied` = `General & Acute...13`
+  )
+
+# Load bed night raw data for the 2nd quater
+GET(
+    bed_night_2,
+    write_disk(bed_night_2 <- tempfile(fileext = ".xlsx"))
+  )
+
+# Loading the data into our enviroment and selecting the neccessary rows
+raw_night_2 <-
+  read_excel(
+    bed_night_2,
+    sheet = "NHS Trust by Sector",
+    skip = 14
+  )
+
+# remove first two entries (one is totals, other is blank)
+beds_night_sliced_2 <-
+  raw_night_2 |>
+  slice(-(1:2))
+
+# Select cols
+beds_night_vars_2 <-
+  beds_night_sliced_2 |>
+  select(
+    `Org Code`,
+    `2_total_beds_available_night`= `Total...6`,
+    `2_general_acute_night_beds_occupied` = `General & Acute...13`
+  )
+
+# Load bed day raw data for the 1st quater
+GET(
+    bed_day_1,
+    write_disk(bed_day_1 <- tempfile(fileext = ".xlsx"))
+  )
+
+# Loading the data into our enviroment and selecting the neccessary rows
+raw_day_1 <-
+  read_excel(
+    bed_day_1,
+    sheet = "NHS Trust by Sector",
+    skip = 14
+  )
+
+# remove first two entries (one is totals, other is blank)
+beds_day_sliced_1 <-
+  raw_day_1 |>
+  slice(-(1:2))
+
+# Select cols
+beds_day_vars_1 <-
+  beds_day_sliced_1 |>
+  select(
+    `Org Code`,
+    `1_total_beds_available_day`= `Total...6`,
+    `1_general_acute_day_beds_occupied` = `General & Acute...13`
+  )
+
+# Load bed night raw data for the 1st quater
+GET(
+    bed_night_1,
+    write_disk(bed_night_1 <- tempfile(fileext = ".xlsx"))
+  )
+
+# Loading the data into our enviroment and selecting the neccessary rows
+raw_night_1 <-
+  read_excel(
+    bed_night_1,
+    sheet = "NHS Trust by Sector",
+    skip = 14
+  )
+
+# remove first two entries (one is totals, other is blank)
+beds_night_sliced_1 <-
+  raw_night_1 |>
+  slice(-(1:2))
+
+# Select cols
+beds_night_vars_1 <-
+  beds_night_sliced_1 |>
+  select(
+    `Org Code`,
+    `1_total_beds_available_night`= `Total...6`,
+    `1_general_acute_night_beds_occupied` = `General & Acute...13`
+  )
+
+
+#Merging day and night bed together for all 4 quaters
 bed_occupancy <-
   beds_day_vars |>
   left_join(
     beds_night_vars,
-    by = "Org Code"
-  )
+    by = "Org Code")|>
+  left_join(
+    beds_day_vars_3,
+    by = "Org Code") |>
+  left_join(
+    beds_night_vars_3,
+    by = "Org Code") |>
+  left_join(
+    beds_day_vars_2,
+    by = "Org Code") |>
+  left_join(
+    beds_night_vars_2,
+    by = "Org Code") |>
+  left_join(
+    beds_day_vars_1,
+    by = "Org Code") |>
+  left_join(
+    beds_night_vars_1,
+    by = "Org Code")
+  
 
 #Merging Discharge data with bed occupancy
 correlation <-
@@ -184,104 +371,15 @@ correlation <-
     by = "Org Code"
   )
 
-#Merging Discharge data with bed occupancy
-correlation_mean <-
-  discharge_mean |>
-  left_join(
-    bed_occupancy,
-    by = "Org Code"
-  )
-
-
 glimpse(correlation)
-glimpse(correlation_mean)
 summary(correlation)
-summary(correlation_mean)
 
-### ERROR
+# Let see which variable is closely correlated with discharge. 
+correlate <- correlate(correlation|>
+          select(where(is.double))
+)
 
-cbind(
-   lapply(
-     lapply(correlation, is.na)
-     , sum)
-   )
+view(correlate)
 
-
-transform(correlation, may_mean = as.numeric(may_mean), may_sum = as.numeric(may_sum), 
-               april_mean = as.numeric(april_mean), april_sum = as.numeric(april_sum), 
-               total_beds_available_day = as.numeric(total_beds_available_day), 
-               general_acute_day_beds_occupied = as.numeric(general_acute_day_beds_occupied),
-               total_beds_available_night = as.numeric(total_beds_available_night), 
-               general_acute_night_beds_occupied = as.numeric(general_acute_night_beds_occupied)
-               )
-cor <- correlation |> 
-        select(may_mean,may_sum,april_mean,april_sum,total_beds_available_day,
-        general_acute_day_beds_occupied,total_beds_available_night,general_acute_night_beds_occupied
-        
-        )
-
-correlate(cor)
-correlate(correlation)
-library(skimr)
-correlate(correlation$may_mean, correlation$general_acute_day_beds_occupied)
-correlate(correlation$april_sum, correlation$general_acute_day_beds_occupied)
-
-typeof(correlation$total_beds_available_day)
-        
-
-
-
-#Select the neccessary variables
-cor <- 
-  correlation |> 
-  select(april_sum, may_sum, total_beds_available_day, general_acute_day_beds_occupied,
-  total_beds_available_night, general_acute_night_beds_occupied
-  )
-
-cor |> skimr::skim()
-
-#Getting to see the correlation
- plot(correlation |>
-          select(april_mean, april_sum, may_mean, may_sum,
-          total_beds_available_day,general_acute_day_beds_occupied,
-          total_beds_available_night,general_acute_night_beds_occupied)
-      )
- plot(correlation_mean |>
-          select(april_mean, may_mean,
-          total_beds_available_day,general_acute_day_beds_occupied,
-          total_beds_available_night,general_acute_night_beds_occupied)
-      )
- correlate(correlation_mean$april_mean,correlation_mean$may_mean,
-          correlation_mean$total_beds_available_night,)
-
- install.packages("skimr")
-correlation |> skimr::skim()
- # Replace '-' character with NA
-#corr <-
-  #correlation |>
-  #mutate(
-   # across(
-    #  .cols = !c(Org Code)
-     # ~ str_replace_all(.x, "-", NA_character_)
-    #)
-  #)
-
-# Drop Na
-  corr_ <-
-  corr |>
-  drop_na()
-
-#Select the neccessary variables
-  corr_no_na <- 
-  corr_ |> 
-  select(april_total, may_total, total_beds_available_day, general_acute_day_beds_occupied,
-  total_beds_available_night, general_acute_night_beds_occupied
-  )
-
-  # Change cols to double
-cor_double <- as.integer(corr_no_na)
-    
-
-#Getting to see the correlation
- plot(corr_double)
- correlate(corr_no_na)
+### Total bed available at night is has a strong correlation with discharge data. 
+### Plus it has sufficent historical data to build a model.
